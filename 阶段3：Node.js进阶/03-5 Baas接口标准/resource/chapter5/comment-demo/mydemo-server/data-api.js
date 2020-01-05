1509a2b1ee14850bb516dcb69e32250e58d6aa09
iceyang <w@icey.cc>
2020-01-06 00:56:18 +0800

    up

--- 阶段3：Node.js进阶/03-5 Baas接口标准/resource/chapter5/comment-demo/mydemo-server/data-api.js
+++ 阶段3：Node.js进阶/03-5 Baas接口标准/resource/chapter5/comment-demo/mydemo-server/data-api.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 // 调用天气接口获取数据
+
 module.exports.getData = async () => {
   let ret = await axios.get('http://www.weather.com.cn/data/cityinfo/101010100.html')
   let info = ret.data.weatherinfo;
